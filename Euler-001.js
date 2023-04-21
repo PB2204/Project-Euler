80d6713fa24c744a12346110d32a1695445a2ed2
Pabitra Banerjee <120979437+PB2204@users.noreply.github.com>
2023-04-22 00:58:18 +0530

    Update Euler-001.js

--- Euler-001.js
+++ Euler-001.js
@@ -1,3 +1,13 @@
+/*
+Project Euler
+
+https://projecteuler.net/problem=1
+
+Multiples Of 3 & 5
+
+by : Pabitra Banerjee [https://pabitrabanerjee.newsgoogle.org]
+*/
+
 function solution(n) {
 
    var r = n / 3 | 0;
@@ -7,3 +17,6 @@ function solution(n) {
    return 3 * r * ++r + 5 * s * ++s - 15 * t * ++t >> 1;
 }
 console.log(solution(999));
+
+
+// https://replit.com/@RockstarPabitra/Euler-001
